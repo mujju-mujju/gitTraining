b8076700e09b06b1b67f9efd425b8516fd5e2944
mujju-mujju <rahmanmujju@gmail.com>
2025-02-28 18:49:55 +0530

    First push

---
+++ src/test/java/git/Class.java
@@ -0,0 +1,25 @@
+package git;
+
+public class Class {
+	private void tamil() {
+		System.out.println("T");
+	}
+	private void English() {
+		System.out.println("E");
+
+	}
+	private void Telugu() {
+		System.out.println("T");
+
+	}
+	public static void main(String[] args) {
+		Class c = new Class();
+		c.tamil();
+		c.English();
+		c.Telugu();
+				
+		
+		
+	}
+
+}
